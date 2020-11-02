5fe5800fc5c4eb51f0e2873903b990ec360d41c8
zbyti <zbyti@yahoo.com>
2020-11-02 11:54:59 +0100

    refactor

--- src/fire.pas
+++ src/fire.pas
@@ -17,11 +17,9 @@ const
 
 var
   b0i      : byte absolute $e0;
-
   p0       : PByte absolute $e1;
   p1       : PByte absolute $e3;
   p2       : PByte absolute $e5;
-
   b1i, tmp : byte;
 
 begin
@@ -29,9 +27,10 @@ begin
   sdlstl := word(@dl); chbas := hi(charset);
   gprior := $40; sdmctl := $21;
 
+  p0 := pointer(charset);
   for b0i := 0 to $f do begin
-    for b1i := 0 to 7 do poke(charset + b1i + b0i * 8, tmp);
-    inc(tmp,$11);
+    for b1i := 0 to 7 do p0[b1i] := tmp;
+    inc(tmp,$11); inc(p0,8);
   end;
 
   FillChar(pointer(screen), $400, 0);
@@ -41,7 +40,7 @@ begin
     p1 := pointer(screen - 31 + $100);
     p2 := pointer(screen - 31 + $200);
 
-    for b0i := 0 to 255 do begin
+    for b0i := 255 downto 0 do begin
       p0^ := byte(p0[30] + p0[31] + p0[32] + p0[63]) shr 2;
       p1^ := byte(p1[30] + p1[31] + p1[32] + p1[63]) shr 2;
       p2^ := byte(p2[30] + p2[31] + p2[32] + p2[63]) shr 2;
